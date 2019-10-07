6bcc3ef7b3524c0f66a20aafa3f5e9f342e17914
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2019-10-07 17:58:15 +0530

    --- Auto-Git Commit ---

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -3,6 +3,7 @@ function fizzBuzz(num){
         if(i%3===0)console.log('Fizz');
         else if(i%5 === 0) console.log('Buzz');
         else if(i%3===0 && i%5===0)console.log('FizzBuzz');
+        else console(i);
     }
 }
 
