ebe98a2aadf5725187fee3f73658100dbf555963
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2019-10-09 01:34:06 +0530

    --- Auto-Git Commit ---

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace datastructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
