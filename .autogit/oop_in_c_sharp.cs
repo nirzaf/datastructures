58692a48d22462b0cd27bfdc00c48c0cea2bacb6
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2019-10-09 00:44:53 +0530

    --- Auto-Git Commit ---

--- .autogit/oop_in_c_sharp.cs
+++ .autogit/oop_in_c_sharp.cs
@@ -8,5 +8,23 @@ namespace oop_in_c_sharp
         {
 
         }
-    }\ 
+    }
+
+    public class ParentClass {  
+    public ParentClass() {  
+        Console.WriteLine("Parent Constructor.");  
+    }  
+    public void print() {  
+        Console.WriteLine("I'm a Parent Class.");  
+    }  
+}  
+public class ChildClass: ParentClass {  
+    public ChildClass() {  
+        Console.WriteLine("Child Constructor.");  
+    }  
+    public static void Main() {  
+        ChildClass child = new ChildClass();  
+        child.print();  
+    }  
+}  
 }
